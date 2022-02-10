e280b135b2ac1e49698853503d2a61b32577dceb
braden shipley <braden@dave.com>
2022-02-10 12:17:12 -0600

    first commit

---
+++ personal/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" default)))
+ '(package-selected-packages
+   (quote
+    (multiple-cursors cmake-mode protobuf-mode counsel-projectile all-the-icons page-break-lines dashboard dockerfile-mode yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
---
+++ transient/history.el
@@ -0,0 +1,11 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-fetch nil)
+ (magit-merge nil
+              ("--ff-only"))
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-rebase
+  ("--autostash"))
+ (magit-revert
+  ("--edit")))
