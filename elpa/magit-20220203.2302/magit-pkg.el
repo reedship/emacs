ad64fbffcf724440d46e2ec25cba393e781b036a
braden shipley <braden@dave.com>
2022-02-11 11:10:50 -0600

    added missing prelude files
    Signed-off-by: braden shipley <braden@dave.com>

---
+++ elpa/magit-20220203.2302/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20220203.2302" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210826")
+    (git-commit "20211004")
+    (magit-section "20211004")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "bab9257d7e859567fa6648f3085f1de17963d4fd" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-section-20220130.2007/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20220130.2007" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210826"))
+  :commit "bab9257d7e859567fa6648f3085f1de17963d4fd" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
